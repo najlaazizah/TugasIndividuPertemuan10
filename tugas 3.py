5e8e1c1ab174434f2439d8f5cc2ba4918aa419b1
najlaazizah <najlaazizah.4@student.upi.edu>
2025-11-05 14:38:13 +0700

    Add files via upload

---
+++ tugas 3.py
@@ -0,0 +1,15 @@
+# Najla Azizah Gunawan
+# 2501796
+# Kelas A
+
+
+nilai = int(input("Masukan nilai :"))
+
+if (nilai >= 90):
+    print(f"Anda mendapatkan nilai A")
+elif (nilai >=80 and nilai <90):
+       print(f"Anda mendapatkan nilai B")
+elif (nilai >=70 and nilai <80):
+      print(f"Anda mendapatkan nilai C")
+else:
+      print("Anda mendapatkan nilai D")             
